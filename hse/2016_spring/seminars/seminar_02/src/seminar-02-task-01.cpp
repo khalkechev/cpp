059d1f763cf771431a9ff09650b21e9b4bd0815d
Roman Khalkechev <romario@romario-osx3.local>
2016-03-12 18:12:31 +0300

    Added solutions for tasks 1 and 2

---
+++ hse/2016_spring/seminars/seminar_02/src/seminar-02-task-01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    if (sizeof(char) <= sizeof(int) && sizeof(int) <= sizeof(double)) {
+        std::cout << "Yes, it is.\n";
+    } else {
+        std::cout << "No, it is not.\n";
+    }
+    return 0;
+}
