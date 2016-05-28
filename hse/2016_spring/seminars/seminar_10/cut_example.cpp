44255363afd41f66ec59665dc21e6c930e9fda1d
Roman Khalkechev <romario@romario-osx3.local>
2016-05-28 18:24:24 +0300

    Added cut example file

---
+++ hse/2016_spring/seminars/seminar_10/cut_example.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <fstream>
+
+
+void Split(const std::string& line,
+           char delimiter,
+           std::vector<std::string>* columns)
+{
+    size_t begin = 0;
+    for (size_t i = 0; i < line.size(); ++i) {
+        if (line[i] == delimiter) {
+            std::string word = line.substr(begin, i - begin);
+            columns->push_back(word);
+            ++i;
+            begin = i;
+        }
+    }
+    std::string word = line.substr(begin);
+    columns->push_back(word);
+}
+
+void Split2(const std::string& line,
+           char delimiter,
+           std::vector<std::string>* columns)
+{
+    std::string word;
+    for (size_t i = 0; i < line.size(); ++i) {
+        if (line[i] != delimiter) {
+            word += line[i];
+        } else {
+            columns->push_back(word);
+            word = "";
+        }
+    }
+    columns->push_back(word);
+}
+
+int main(int argc, char const *argv[]) {
+
+    int columnNumber = 1;
+    char delimiter = ',';
+    std::string fileName = "example.csv";
+
+    std::ifstream inputFile(fileName);
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::vector<std::string> columns;
+        Split2(line, delimiter, &columns);
+        if (columnNumber - 1 < columns.size() && columnNumber - 1 >= 0) {
+            std::cout << columns[columnNumber - 1] << std::endl;
+        }
+    }
+
+    return 0;
+}
